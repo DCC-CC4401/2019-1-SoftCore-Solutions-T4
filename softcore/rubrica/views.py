13af94d5104155b527193e84d88a285e7e3df1cb
Antonio Montalva <a.montalva7@gmail.com>
2019-05-17 01:27:56 -0400

    commit de prueba

--- softcore/rubrica/views.py
+++ softcore/rubrica/views.py
@@ -17,11 +17,11 @@ def rubricas(request):
 	return render(request, 'rubrica/rubricas.html', context)
 
 def ficha(request, rubrica_id):
-	#NivelCumplimiento.objects.order_by('puntaje')
 	rubrica = get_object_or_404(Rubrica, pk=rubrica_id)
 	criterios = Criterio.objects.filter(rubrica=rubrica_id)
 	max_puntaje = 0
 	critKey = criterios.first().id
+	#aca se buscan todos los niveles de aprobacion
 	for crit in criterios:
 		#se sabe que el primer elemento que se saque de NivelCumplimiento es el mayor posible
 		temp = NivelCumplimiento.objects.filter(criterio = crit.id).first()
@@ -30,4 +30,7 @@ def ficha(request, rubrica_id):
 			critKey = crit.id
 
 	niveles = NivelCumplimiento.objects.filter(criterio=critKey)
+	#el contexto lleva la rubrica en cuestion, un set de niveles que es la primera file de la tabla
+	#ademas de los criterios a partir de los cuales se contruye el resto de la tabla
+	#de esta forma el manejo de la contruccion en el template es mas limpio
 	return render(request, 'rubrica/ficha_rubrica.html', {'rubrica': rubrica, 'criterios': criterios, 'niveles':niveles})
