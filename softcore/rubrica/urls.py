68b18d198d95c1d12ea4ef0e11b751998ccccbbe
Antonio Montalva <a.montalva7@gmail.com>
2019-05-18 17:40:54 -0400

    se anhaden mas funcionalidades a los botones de los interfaces de rubricas, y se anhade el template para crear un rubrica nueva

--- softcore/rubrica/urls.py
+++ softcore/rubrica/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.rubricas, name='rubricas'),
     path('<int:rubrica_id>/', views.ficha, name='ficha'),
+    path('<int:rubrica_id>/edit/', views.edit_ficha, name='edit_ficha'),
+    path('new/', views.new_ficha, name='new_ficha')
 ]
--- softcore/rubrica/views.py
+++ softcore/rubrica/views.py
@@ -34,3 +34,25 @@ def ficha(request, rubrica_id):
 	#ademas de los criterios a partir de los cuales se contruye el resto de la tabla
 	#de esta forma el manejo de la contruccion en el template es mas limpio
 	return render(request, 'rubrica/ficha_rubrica.html', {'rubrica': rubrica, 'criterios': criterios, 'niveles':niveles})
+
+def edit_ficha(request, rubrica_id):
+	rubrica = get_object_or_404(Rubrica, pk=rubrica_id)
+	criterios = Criterio.objects.filter(rubrica=rubrica_id)
+	max_puntaje = 0
+	critKey = criterios.first().id
+	#aca se buscan todos los niveles de aprobacion
+	for crit in criterios:
+		#se sabe que el primer elemento que se saque de NivelCumplimiento es el mayor posible
+		temp = NivelCumplimiento.objects.filter(criterio = crit.id).first()
+		if(temp.puntaje >= max_puntaje):
+			max_puntaje = temp.puntaje
+			critKey = crit.id
+
+	niveles = NivelCumplimiento.objects.filter(criterio=critKey)
+	#el contexto lleva la rubrica en cuestion, un set de niveles que es la primera file de la tabla
+	#ademas de los criterios a partir de los cuales se contruye el resto de la tabla
+	#de esta forma el manejo de la contruccion en el template es mas limpio
+	return render(request, 'rubrica/edit_ficha.html', {'rubrica': rubrica, 'criterios': criterios, 'niveles':niveles})
+
+def new_ficha(request):
+	return render(request, 'rubrica/new_ficha.html', None)
