ab20c7b59149dc391b402a1226dbfe548beed62d
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-05-28 18:58:56 -0400

    se annade la vista de la lista de evaluaciones

--- softcore/evaluacion/views.py
+++ softcore/evaluacion/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+
+
+from .models import Evaluacion, Presentacion, Puntaje, LinksResumen
 
 # Create your views here.
+
+class AdminPageView(TemplateView):
+    template_name = 'index.html'
+
+
+def evaluaciones(request):
+	evaluaciones_list = Evaluacion.objects.order_by('-fecha_termino')[:5]
+	context = {'evaluaciones_list': evaluaciones_list}
+	return render(request, 'evaluacion/evaluaciones.html', context)
+
+#TODO 
+#def fichaEvaluacion(request, evaluacion_id):
+#    evaluacion = get_object_or_404(Evaluacion, pk=evaluacion_id)
+#
