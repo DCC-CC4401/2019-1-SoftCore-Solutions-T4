10213161ac066a4729720ee9c6a23b2eed210770
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-05-28 16:28:21 -0400

    se dan los permisos de admin para crear objetos del modelo de la evaluacion en la bbdd

--- softcore/evaluacion/admin.py
+++ softcore/evaluacion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Evaluacion, Presentacion, Puntaje, Linksresumen
+
+admin.site.register(Evaluacion)
+admin.site.register(Presentacion)
+admin.site.register(Puntaje)
+admin.site.register(Linksresumen)
+
 # Register your models here.
