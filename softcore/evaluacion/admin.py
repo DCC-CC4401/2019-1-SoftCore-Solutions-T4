5839bab0056540edc8131f4294f647e13e278c52
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-05-28 16:49:37 -0400

    se corrige error de tipeo

--- softcore/evaluacion/admin.py
+++ softcore/evaluacion/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Evaluacion, Presentacion, Puntaje, Linksresumen
+from .models import Evaluacion, Presentacion, Puntaje, LinksResumen
 
 admin.site.register(Evaluacion)
 admin.site.register(Presentacion)
 admin.site.register(Puntaje)
-admin.site.register(Linksresumen)
+admin.site.register(LinksResumen)
 
 # Register your models here.
