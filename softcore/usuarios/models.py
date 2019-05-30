8a9d70799b5ec6027b5008ca40dce4d26c435699
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-05-30 17:19:44 -0400

    se agregan los profesores al modelo

--- softcore/usuarios/models.py
+++ softcore/usuarios/models.py
@@ -6,7 +6,15 @@ class CustomUser(AbstractUser):
     is_evaluador = models.BooleanField(default=false)
     is_profesor = models.BooleanField(default=false)
     
-class evaluador(models.Model):
+class Evaluador(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    #todo como extender la password y auth de django ?    
+    correo = models.EmailField(primary_key = True)
+
+    nombre = models.CharField(max_length=80)
+    apellido = models.CharField(max_length=80)
+
+class Profesor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     #todo como extender la password y auth de django ?    
     correo = models.EmailField(primary_key = True)
