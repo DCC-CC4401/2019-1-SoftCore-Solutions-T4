27302e34d44a93b72d949eaa6be12b452e8ef8b8
Tomas Vallejos <tomas.vallejos12@gmail.com>
2019-05-30 19:48:29 -0400

    vista para mostrar lista de evaluadores

--- softcore/usuarios/views.py
+++ softcore/usuarios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
 
+
+from .models import Evaluador
 # Create your views here.
+
+class AdminPageView(TemplateView):
+	template_name = 'index.html'
+
+def evaluadores(request):
+	evaluadores_list = Evaluador.objects.all()
+	context = {'evaluadores_list': evaluadores_list}
+	return render(request, 'usuarios/evaluadores.html', context)
