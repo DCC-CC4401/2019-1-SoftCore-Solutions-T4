22a9bc3080e9a37b95e8ae33c98f01ad8dcb7374
fdzr <fdzr9106@gmail.com>
2019-06-02 18:51:59 -0400

    Agregando ccodigo para que guarde los usuarios a traves del landing page de administrador

--- softcore/usuarios/views.py
+++ softcore/usuarios/views.py
@@ -6,11 +6,25 @@ from django.shortcuts import get_object_or_404
 from .models import CustomUser
 # Create your views here.
 
+
 class AdminPageView(TemplateView):
-	template_name = 'index.html'
+    template_name = 'index.html'
+
 
 def evaluadores(request):
-	#get_user_model().objects.filter(profesor = False)
-	evaluadores_list = CustomUser.objects.filter(profesor = False)
-	context = {'evaluadores_list': evaluadores_list}
-	return render(request, 'usuarios/evaluadores.html', context)
+    if request.method == 'POST':
+        username = request.POST['username']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        tipo_usuario = request.POST['tipo_usuario']
+
+        if tipo_usuario == 'profesor':
+            CustomUser.objects.create(correo=email, nombre=username,
+                                      apellido=apellido, profesor=True)
+        else:
+            CustomUser.objects.create(correo=email, nombre=username,
+                                      apellido=apellido, evaluador=True)
+
+    evaluadores_list = CustomUser.objects.all()
+    context = {'evaluadores_list': evaluadores_list}
+    return render(request, 'usuarios/evaluadores1.html', context)
